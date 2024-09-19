d7f363cdb50a8a359accca093788e9a47ead2877
Pranav RK <pranavrk7@gmail.com>
2024-09-19 21:50:01 +0530

    nvim: add markdown specific plugins

--- nvim/.config/nvim/lua/radar/plugins/markdown.lua
+++ nvim/.config/nvim/lua/radar/plugins/markdown.lua
@@ -1,4 +1,29 @@
 return {
-  "vim-pandoc/vim-pandoc-syntax",
-  event = { "BufReadPre", "BufNewFile" },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      file_types = { "markdown", "norg", "rmd", "org" },
+      code = {
+        sign = true,
+        width = "block",
+        right_pad = 1,
+      },
+      heading = {
+        sign = true,
+        icons = {},
+      },
+    },
+    ft = { "markdown", "norg", "rmd", "org" },
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
+    ft = { "markdown" },
+  },
 }
