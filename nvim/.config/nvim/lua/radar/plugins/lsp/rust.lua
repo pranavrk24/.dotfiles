475dd8d08daa40f54720a720ed0e39a82ea25d89
Pranav RK <pranavrk7@gmail.com>
2024-09-21 11:21:45 +0530

    nvim: add rust plugins

--- nvim/.config/nvim/lua/radar/plugins/lsp/rust.lua
+++ nvim/.config/nvim/lua/radar/plugins/lsp/rust.lua
@@ -1,5 +1,10 @@
 return {
   {
+    "mrcjkb/rustaceanvim",
+    version = "^5", -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
+  {
     "saecki/crates.nvim",
     event = { "BufRead Cargo.toml" },
     config = function()
