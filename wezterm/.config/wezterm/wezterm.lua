ddc67be8a92411d90158c84022bc231ccdce3b7d
Pranav RK <pranavrk7@gmail.com>
2024-10-04 22:44:32 +0530

    chore: cleanup

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,44 +7,21 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Rosé Pine (Gogh)"
 
 config.font_size = 14
 config.font = wezterm.font_with_fallback({
 	{ family = "FiraCode Nerd Font", weight = 450 },
-	{ family = "JetBrains Mono Nerd Font", weight = "Regular" },
+	{ family = "Iosevka Nerd Font", weight = "Regular" },
 	{ family = "ZedMono Nerd Font", weight = "Regular" },
-	{ family = "Iosevka Nerd Font", weight = "Medium" },
+	{ family = "JetBrains Mono Nerd Font", weight = "Regular" },
 	{ family = "Mononoki Nerd Font", weight = "Regular" },
-	{ family = "Monaspace Neon", weight = "Medium" },
-	{ family = "Monaspace Argon", weight = "Regular" },
-	{ family = "Monaspace Krypton", weight = "Regular" },
-	{ family = "Monaspace Xenon", weight = "Regular" },
-	{ family = "Monaspace Radon", weight = "Regular" },
 })
 
 -- FiraCode
 config.harfbuzz_features = { "zero", "cv02", "ss02", "ss03", "ss05" }
 
--- config.font_rules = {
--- 	{
--- 		intensity = "Bold",
--- 		italic = true,
--- 		font = wezterm.font({ family = "Maple Mono NF", weight = "Bold", style = "Italic" }),
--- 	},
--- 	{
--- 		italic = true,
--- 		intensity = "Half",
--- 		font = wezterm.font({ family = "Maple Mono NF", weight = "Regular", style = "Italic" }),
--- 	},
--- 	{
--- 		italic = true,
--- 		intensity = "Normal",
--- 		font = wezterm.font({ family = "Maple Mono NF", weight = "Regular", style = "Italic" }),
--- 	},
--- }
-
-config.window_background_opacity = 0.9
+-- config.window_background_opacity = 0.9
 
 config.warn_about_missing_glyphs = false
 
