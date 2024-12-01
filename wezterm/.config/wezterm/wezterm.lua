be112c350903f512a465e8f6da9ce4eabe45f935
Pranav RK <pranavrk7@gmail.com>
2024-12-01 12:50:29 +0530

    updates

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,7 +7,7 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.color_scheme = "GruvboxDarkHard"
+config.color_scheme = "kanagawabones"
 
 config.font_size = 14
 config.font = wezterm.font_with_fallback({
